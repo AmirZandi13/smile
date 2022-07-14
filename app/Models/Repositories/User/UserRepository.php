6d4efd282b727a4d68cc51adb65e804a5e18ce70
amirzandi13 <amirzandieh1@gmail.com>
2022-07-14 15:23:47 +0430

    feat(repository): add base and user repositories

---
+++ app/Models/Repositories/User/UserRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models\Repositories\User;
+
+use App\Models\Repositories\BaseRepository;
+
+class UserRepository extends BaseRepository implements UserRepositoryInterface
+{
+}
