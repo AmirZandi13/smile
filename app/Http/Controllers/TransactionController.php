3f3a55d92d9a1d135a621a7eb257d81cdbaf93ef
amirzandi13 <amirzandieh1@gmail.com>
2022-07-16 13:25:08 +0430

    feat(documentation): api documentations & swagger and curls

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\TransactionsRequest;
 use App\Http\Resources\TransactionsResource;
 use App\Services\BankServiceInterface;
-use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class TransactionController extends Controller
@@ -19,9 +18,29 @@ class TransactionController extends Controller
     }
 
     /**
-     * Display the resources.
-     *
-     * @return JsonResponse
+     * @OA\Get(
+     *     path="/api/v1/transactions",
+     *     summary="Get the user's transactions",
+     *     tags={"Transaction"},
+     *     @OA\Parameter(
+     *         description="Authorization token",
+     *         in="header",
+     *         name="authorization",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Parameter(
+     *         description="Account number",
+     *         in="query",
+     *         name="account_number",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK"
+     *     )
+     * )
      */
     public function index(TransactionsRequest $request)
     {
